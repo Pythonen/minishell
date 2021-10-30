9608e72e461bcc27d69789969301283be3f5dbdd
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2021-10-30 14:06:46 +0300

    working with static commands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use nix::sys::wait::waitpid;
+use nix::unistd::ForkResult;
+use nix::unistd::{execvp, fork};
+use std::ffi::CStr;
+
 fn main() {
-    println!("Hello, world!");
+    match unsafe { fork() } {
+        Ok(ForkResult::Child) => {
+            execvp(
+                CStr::from_bytes_with_nul(b"ls\0").expect("command failed"),
+                &[CStr::from_bytes_with_nul(b"-l\0").expect("args failed")],
+            )
+            .expect("exec failed");
+        }
+        Ok(ForkResult::Parent { child }) => {
+            waitpid(child, None).unwrap();
+        }
+        Err(err) => panic!(
+            "Could not create new child process and it caused error {}",
+            err
+        ),
+    }
 }
